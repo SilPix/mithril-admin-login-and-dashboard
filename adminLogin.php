1e375f53114d3cdd8e3198fa205c08aefb62ef1c
SilPix <99146270+SilPix@users.noreply.github.com>
2024-06-26 13:52:31 +0100

    Added User-Login-To-Dashboard Functionality
    - User can now login to the "Admin" Dashboard with their email and password (Although, the Super-Admin ID and Password still works)
    - Users can only see their own record(s) when they log in to the dashboard. Only the Super-Admin can see all records
    - Trying to register using an email that's been used before will cause an error and crash (thanks to SQL's 'UNIQUE' clause)
    
    The next logical task is to add proper error handling and notify users that the email has been used, instead of a janky PHP error.

--- adminLogin.php
+++ adminLogin.php
@@ -20,45 +20,102 @@
 
 			<br/> <br/>
 
-			<label for="email">Admin ID:</label><br/>
-			<input type="text" id="unique" name="unique" placeholder="XXX-XXX" required><br/>
+			<label for="admin_id">Admin ID:</label><br/>
+			<input type="text" id="admin_id" name="admin_id" placeholder="XXX-XXX" required><br/>
 
 			<label for="password">Admin Password:</label><br/>
-			<input type="password" id="password" name="password" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;" required><br/>
+			<input type="password" id="admin_password" name="admin_password" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;" required><br/>
 
 			<?php
+			//General ID & Password
 			$admin_id = "ADR123";
 			$admin_pw = "admin";
 			$adm_check_1 = $adm_check_2 = false;
 
-			if(isset($_POST['unique'])){
-				if($_POST['unique'] == $admin_id){
-					$adm_check_1 = true;
-				}
-				elseif($_POST['unique'] != $admin_id){
-					$adm_check_1 = false;
-					echo "<br/>Error: Not an Administrator's ID <br/>";
-				}
-			}
+			//SQL Variables
+			$server = 'localhost';
+			$username = "root";
+			$password = '';
+			$db = "users_task1";
 
-			if(isset($_POST['password'])){
-				if($_POST['password'] == $admin_pw){
-					$adm_check_2 = true;
-				}
-				elseif($_POST['password'] != $admin_pw){
-					$adm_check_2 = false;
-					echo "Error: Not an Administrator's Password";
-				}
-			}
+			try{
+				//Using PDO, in 'exception' mode
+				$conn = new PDO("mysql:host=$server; dbname=$db", $username, $password);
+				$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+				//Prepare SQL Statements
+				$sql = $conn->prepare("SELECT email, password FROM user_info");
 
-			if($adm_check_1 && $adm_check_2){
-				//Mark user as authorized
-				session_start();
-				$_SESSION['authenticate'] = true;
+				$sql->execute();
 
-				//Redirect
-				header("Location: adminDashboard.php");
-				exit;
+				$data = $sql->fetchAll();
+
+				//Only allowed through is the given ID & Password matches our list
+				if(isset($_POST['admin_id'])){
+					foreach ($data as $row) {
+						if($_POST['admin_id'] == $row['email']){
+							$adm_check_1 = true;
+						}
+					}
+
+					if($_POST['admin_id'] == $admin_id){
+						$adm_check_1 = true;
+					}
+
+					if(!$adm_check_1){ echo "<br/>Error: Not an Administrator's ID <br/>"; }
+				}
+
+				if(isset($_POST['admin_password'])){
+					foreach ($data as $row) {
+						if($_POST['admin_password'] == $row['password']){
+							$adm_check_2 = true;
+						}
+					}
+
+					if($_POST['admin_password'] == $admin_pw){
+						$adm_check_2 = true;
+					}
+
+					if(!$adm_check_2){ echo "Error: Not an Administrator's Password"; }
+				}
+
+				//Check that the Admin's Password matches his/her ID
+				if(isset($_POST['admin_id']) && isset($_POST['admin_password'])){
+					$sql = $conn->prepare("SELECT password FROM user_info WHERE email=:checkid");
+					$sql->bindParam(":checkid", $_POST['admin_id']);
+					$sql->execute();
+
+					$data= null;
+					$data = $sql->fetchAll();
+
+					foreach($data as $row){
+						//If given password doesn't match stored password
+						if($_POST['admin_password'] != $row['password']){
+							$adm_check_1 = $adm_check_2 = false;
+							throw new Exception();	
+						}
+					}
+				}
+
+				if($adm_check_1 && $adm_check_2){
+					//Mark user as authorized
+					session_start();
+					$_SESSION['authenticate'] = true;
+
+					//Check for Super-Admin
+					$_SESSION['admin_id'] = $_POST['admin_id'];
+					$_SESSION['admin_password'] = $_POST['admin_password'];
+
+					//Redirect
+					header("Location: adminDashboard.php");
+					exit;
+				}
+			} catch(PDOException $e){
+				echo $sql . "<br/>" . $e->getMessage();
+				$conn = null;
+			} catch(Exception $e1){
+				echo "<br/>The ID and Password do not match <br/>";
 			}
 			?>
 
